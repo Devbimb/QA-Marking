afd482263e9eb87d50cc510d4fe32479793097e8
Devbimb <yenibimbola@gmail.comssh-keygen -t rsa -f ~/.ssh/id_rsa  ygit config --global user.email yenibimbola@gmail.com>
2022-08-02 22:11:45 +0100

    QA-Marking

---
+++ Excercise-CRUD/src/main/java/com/example/accountdto/AccountDTO.java
@@ -0,0 +1,17 @@
+package com.example.accountdto;
+
+import com.example.accountmodel.CustomerAct;
+
+public class AccountDTO {
+	
+    private int id;
+
+    private String name;
+    
+    private int actno;
+
+	public AccountDTO() {
+		super();
+	}
+
+ }
